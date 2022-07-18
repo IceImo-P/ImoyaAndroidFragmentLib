fb1b8390c87d1a45d94267c1ca2f8d2fd39ebb6d
IceImo-P <imoya.ice@gmail.com>
2022-07-19 00:39:20 +0900

    Release 1.4.0 - Add getBundle method to ArgumentsUtil - Add roundTripContainerId property to RoundTripClientFragment - Remove unused code (RoundTripHost class)

--- lib/src/main/java/net/imoya/android/fragment/ArgumentsUtil.kt
+++ lib/src/main/java/net/imoya/android/fragment/ArgumentsUtil.kt
@@ -27,4 +27,24 @@ object ArgumentsUtil {
             fragment.arguments = arguments
         }
     }
+
+    /**
+     * [Fragment] の arguments に設定された、指定の [Bundle] を取得します。
+     * arguments または指定の [Bundle] が設定されていない場合は、
+     * [IllegalArgumentException] を throw します。
+     *
+     * @param fragment 対象の [Fragment]
+     * @param key arguments に保存されている [Bundle] の key
+     * @param callback 指定の [Bundle] が引数となるコールバック
+     * @throws IllegalArgumentException [fragment] に arguments が設定されていないか、
+     * [key] に紐づく [Bundle] が arguments に設定されていません。
+     */
+    @JvmStatic
+    fun getBundle(fragment: Fragment, key: String, callback: (bundle: Bundle) -> Unit) {
+        val arguments = fragment.arguments
+            ?: throw IllegalArgumentException("Arguments are not set")
+        val bundle = arguments.getBundle(key)
+            ?: throw IllegalArgumentException("Argument $key is not set")
+        callback(bundle)
+    }
 }
